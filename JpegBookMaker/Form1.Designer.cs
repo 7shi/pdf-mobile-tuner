f46cfcfc7cb0e1c1c58d096684ac0a55796c18f4
7shi <7shi@live.jp>
2011-02-01 20:53:19 +0900

    first commit

---
+++ JpegBookMaker/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace JpegBookMaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox2);
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Panel2.Resize += new System.EventHandler(this.splitContainer1_Panel2_Resize);
+            this.splitContainer1.Size = new System.Drawing.Size(751, 530);
+            this.splitContainer1.SplitterDistance = 96;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(751, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 554);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(751, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.fileToolStripMenuItem.Text = "ファイル(&F)";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "終了(&X)";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.CheckBoxes = true;
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(96, 530);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.SmallIcon;
+            this.listView1.ItemChecked += new System.Windows.Forms.ItemCheckedEventHandler(this.listView1_ItemChecked);
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(86, 118);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(95, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(86, 118);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.Description = "JPEGフォルダを選択してください。";
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "開く(&O)";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 576);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "JPEG Book Maker";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ JpegBookMaker/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace JpegBookMaker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            AdjustPanel();
+            pictureBox1.MouseWheel += pictureBox_MouseWheel;
+            pictureBox2.MouseWheel += pictureBox_MouseWheel;
+        }
+
+        private void pictureBox_MouseWheel(object sender, MouseEventArgs e)
+        {
+            var fi = listView1.FocusedItem;
+            if (fi == null && listView1.Items.Count > 0)
+                fi = listView1.Items[0];
+            if (fi == null) return;
+
+            int index = fi.Index;
+            int count = listView1.Items.Count;
+            bool ok = true;
+            for (int i = 0; i < 2; )
+            {
+                if (e.Delta > 0) index--; else index++;
+                if (index < 0 || index >= count)
+                {
+                    ok = false;
+                    break;
+                }
+                if (listView1.Items[index].Checked) i++;
+            }
+            if (ok)
+            {
+                stop = true;
+                fi = listView1.Items[index];
+                listView1.FocusedItem = fi;
+                listView1.EnsureVisible(index);
+                ShowPage(fi);
+                stop = false;
+            }
+            listView1.Focus();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void splitContainer1_Panel2_Resize(object sender, EventArgs e)
+        {
+            AdjustPanel();
+        }
+
+        private void AdjustPanel()
+        {
+            var sz = splitContainer1.Panel2.ClientSize;
+            int w = sz.Width / 2;
+            pictureBox1.Bounds = new Rectangle(0, 0, w, sz.Height);
+            pictureBox2.Bounds = new Rectangle(w, 0, sz.Width - w, sz.Height);
+        }
+
+        bool stop = false;
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog(this) != DialogResult.OK) return;
+
+            stop = true;
+            SetBitmap(null, null);
+            listView1.Items.Clear();
+            listView1.BeginUpdate();
+            var dir = folderBrowserDialog1.SelectedPath;
+            toolStripStatusLabel1.Text = dir;
+            var files = Directory.GetFiles(dir, "*.jpg");
+            foreach (var file in files)
+            {
+                var fn = Path.GetFileNameWithoutExtension(file);
+                listView1.Items.Add(new ListViewItem(fn) { Tag = file, Checked = true });
+            }
+            listView1.EndUpdate();
+            if (listView1.Items.Count > 0)
+            {
+                var fi = listView1.Items[0];
+                listView1.FocusedItem = fi;
+                ShowPage(fi);
+            }
+            stop = false;
+        }
+
+        private void ShowPage(ListViewItem li)
+        {
+            if (li == null)
+            {
+                SetBitmap(null, null);
+                return;
+            }
+
+            var stp = stop;
+            stop = true;
+            var first = true;
+            ListViewItem li1 = null, li2 = null;
+            foreach (ListViewItem li3 in listView1.Items)
+            {
+                if (!li3.Checked) continue;
+                if (!first && li1 == null)
+                    li1 = li3;
+                else
+                {
+                    li2 = li3;
+                    if (li3.Index >= li.Index) break;
+                    li1 = li2 = null;
+                    first = false;
+                }
+            }
+            string path1 = null, path2 = null;
+            foreach (ListViewItem li4 in listView1.Items)
+                if (li4.Selected) li4.Selected = false;
+            if (li1 != null)
+            {
+                li1.Selected = true;
+                path1 = li1.Tag.ToString();
+            }
+            if (li2 != null)
+            {
+                li2.Selected = true;
+                path2 = li2.Tag.ToString();
+            }
+            SetBitmap(path1, path2);
+            stop = stp;
+        }
+
+        string bmpPath1, bmpPath2;
+
+        private void SetBitmap(string path1, string path2)
+        {
+            var cur = Cursor.Current;
+            Cursor.Current = Cursors.WaitCursor;
+
+            var b1 = pictureBox1.Image;
+            var b2 = pictureBox2.Image;
+            Image bmp1 = null, bmp2 = null;
+            if (path1 == bmpPath1)
+                bmp1 = b1;
+            else if (path1 == bmpPath2)
+                bmp1 = b2;
+            else if (path1 != null)
+                bmp1 = new Bitmap(path1);
+            if (path2 == bmpPath2)
+                bmp2 = b2;
+            else if (path2 == bmpPath1)
+                bmp2 = b1;
+            else if (path2 != null)
+                bmp2 = new Bitmap(path2);
+            bmpPath1 = path1;
+            bmpPath2 = path2;
+            if (b1 != bmp1) pictureBox1.Image = bmp1;
+            if (b2 != bmp2) pictureBox2.Image = bmp2;
+            if (b1 != null && b1 != bmp1 && b1 != bmp2) b1.Dispose();
+            if (b2 != null && b2 != bmp1 && b2 != bmp2) b2.Dispose();
+
+            Cursor.Current = cur;
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!stop) ShowPage(listView1.FocusedItem);
+        }
+
+        private void listView1_ItemChecked(object sender, ItemCheckedEventArgs e)
+        {
+            if (!stop) ShowPage(listView1.FocusedItem);
+        }
+    }
+}
